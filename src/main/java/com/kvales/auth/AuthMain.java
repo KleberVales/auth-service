68b5986cfbcb4d7054c6c35c3962e9b81ace9ba0
Kleber Vales <klebervales.dev@gmail.com>
2026-09-05 16:21:11 -0300

    feat: structure of new feature

--- src/main/java/com/kvales/auth/AuthMain.java
+++ src/main/java/com/kvales/auth/AuthMain.java
@@ -1,4 +1,18 @@
 package com.kvales.auth;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
 public class AuthMain {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(AuthMain.class, args);
+
+    }
+
 }
---
+++ src/main/java/com/kvales/auth/application/service/AuthenticateService.java
@@ -0,0 +1,47 @@
+package com.kvales.auth.application.service;
+
+import com.kvales.auth.application.port.in.AuthenticateUseCase;
+import org.springframework.stereotype.Service;
+
+import com.kvales.auth.application.port.out.UserProvider;
+import com.kvales.auth.application.port.out.PasswordHasher;
+import com.kvales.auth.application.port.out.TokenGenerator;
+
+@Service
+public class AuthenticateService
+        implements AuthenticateUseCase {
+
+    private final UserProvider userProvider;
+    private final PasswordHasher passwordHasher;
+    private final TokenGenerator tokenGenerator;
+
+    public AuthenticateService(
+            UserProvider userProvider,
+            PasswordHasher passwordHasher,
+            TokenGenerator tokenGenerator
+    ) {
+        this.userProvider = userProvider;
+        this.passwordHasher = passwordHasher;
+        this.tokenGenerator = tokenGenerator;
+    }
+
+    @Override
+    public AuthenticateResult authenticate(
+            AuthenticateCommand command
+    ) {
+
+        var user =
+                userProvider.findByEmail(command.email());
+
+        if (!passwordHasher.matches(
+                command.password(),
+                user.passwordHash()
+        )) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        var token = tokenGenerator.generate(user);
+
+        return new AuthenticateResult(token);
+    }
+}
